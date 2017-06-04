79b92ba23462ff4eff668c9b5e4c62df364c8dcf
Grace Stuart <gracerstuart@gmail.com>
2017-06-04 09:35:56 -0700

    add about activity

--- app/src/main/java/com/epicodus/whatsinthefridge_android/About.java
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/About.java
@@ -1,13 +1,28 @@
 package com.epicodus.whatsinthefridge_android;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class About extends AppCompatActivity {
+    @Bind(R.id.aboutTitle) TextView mAboutTitle;
+    @Bind(R.id.aboutDescription) TextView mAboutDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
+
+        Typeface boldText = Typeface.createFromAsset(getAssets(), "fonts/caviar_dreams_bold.ttf");
+        mAboutTitle.setTypeface(boldText);
+
+        Typeface Text = Typeface.createFromAsset(getAssets(), "fonts/caviar_dreams.ttf");
+        mAboutDescription.setTypeface(boldText);
+
     }
 }
