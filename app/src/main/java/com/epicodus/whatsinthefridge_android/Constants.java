85fbb6107268c7daa873a0b9d564123ac97fb526
Grace Stuart <gracerstuart@gmail.com>
2017-06-04 11:15:10 -0700

    add query parameter

--- app/src/main/java/com/epicodus/whatsinthefridge_android/Constants.java
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/Constants.java
@@ -3,4 +3,5 @@ package com.epicodus.whatsinthefridge_android;
 
 public class Constants {
     public static final String API_URL = BuildConfig.API_URL;
+    public static final String API_INGREDENT_QUERY_PARAMETER = "i";
 }
