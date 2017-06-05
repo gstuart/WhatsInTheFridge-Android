e4a11e756dda345a6b0a4139bd0e34e5916cd7b2
Grace Stuart <gracerstuart@gmail.com>
2017-06-05 11:18:49 -0700

    add functionality

--- app/src/main/java/com/epicodus/whatsinthefridge_android/ui/RecipeDetailFragment.java
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/ui/RecipeDetailFragment.java
@@ -6,22 +6,54 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.epicodus.whatsinthefridge_android.R;
+import com.epicodus.whatsinthefridge_android.models.Recipe;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class RecipeDetailFragment extends Fragment {
+    @Bind(R.id.restaurantImageView) ImageView mRestaurantImageView;
+    @Bind(R.id.recipeTitleTextView) TextView mRecipeTitleTextView;
+    @Bind(R.id.ingredientListTextView) TextView mIngredientListTextView;
+    @Bind(R.id.websiteTextView) TextView mWebsiteTextView;
 
+    private Recipe mRecipe;
 
-    public RecipeDetailFragment() {
-        // Required empty public constructor
+    public static RecipeDetailFragment newInstance(Recipe recipe) {
+        RecipeDetailFragment recipeDetailFragment = new RecipeDetailFragment();
+        Bundle args = new Bundle();
+        args.putParcelable("recipe", Parcels.wrap(recipe));
+        recipeDetailFragment.setArguments(args);
+        return recipeDetailFragment;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRecipe = Parcels.unwrap(getArguments().getParcelable("recipe"));
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_recipe_detail, container, false);
+        ButterKnife.bind(this, view);
+
+        Picasso.with(view.getContext()).load(mRecipe.getImageUrl()).into(mRestaurantImageView);
+
+        mRecipeTitleTextView.setText(mRecipe.getTitle());
+        mWebsiteTextView.setText(mRecipe.getLink());
+        mIngredientListTextView.setText(mRecipe.getIngredients());
+
+        return view;
     }
 
 }
