e1c91048967fa4b3cfa24a14962745ed75ad85f0
Grace Stuart <gracerstuart@gmail.com>
2017-06-05 09:48:10 -0700

    set up look of fragment

---
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/ui/RecipeDetailFragment.java
@@ -0,0 +1,27 @@
+package com.epicodus.whatsinthefridge_android.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.epicodus.whatsinthefridge_android.R;
+
+public class RecipeDetailFragment extends Fragment {
+
+
+    public RecipeDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipe_detail, container, false);
+    }
+
+}
