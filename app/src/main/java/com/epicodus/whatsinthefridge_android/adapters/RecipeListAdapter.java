4ef4cf53ba3cf73316e7199eab7f52b784d456f8
Grace Stuart <gracerstuart@gmail.com>
2017-06-05 07:07:29 -0700

    add recipelist adapter

--- app/src/main/java/com/epicodus/whatsinthefridge_android/adapters/RecipeListAdapter.java
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/adapters/RecipeListAdapter.java
@@ -1,5 +1,67 @@
 package com.epicodus.whatsinthefridge_android.adapters;
 
 
-public class RecipeListAdapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.epicodus.whatsinthefridge_android.R;
+import com.epicodus.whatsinthefridge_android.models.Recipe;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.RecipeListViewHolder> {
+    private ArrayList<Recipe> mRecipes = new ArrayList<>();
+    private Context mContext;
+
+    public RecipeListAdapter(Context context, ArrayList<Recipe> recipes) {
+        mContext = context;
+        mRecipes = recipes;
+    }
+
+    @Override
+    public RecipeListAdapter.RecipeListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item, parent, false);
+        RecipeListViewHolder viewHolder = new RecipeListViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecipeListAdapter.RecipeListViewHolder holder, int position) {
+        holder.bindRecipe(mRecipes.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRecipes.size();
+    }
+
+    public class RecipeListViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.recipeImageView) ImageView mRecipeImageView;
+        @Bind(R.id.recipeTitleTextView) TextView mRecipeTitleTextView;
+        @Bind(R.id.recipeUrlTextView) TextView mRecipeUrlTextView;
+        @Bind(R.id.recipeIngredientsTextView) TextView mRecipeIngredientsTextView;
+
+        private Context mContext;
+
+        public RecipeListViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindRecipe(Recipe recipe) {
+            mRecipeTitleTextView.setText(recipe.getTitle());
+            mRecipeUrlTextView.setText(recipe.getLink());
+            mRecipeIngredientsTextView.setText(recipe.getIngredients());
+
+        }
+    }
 }
