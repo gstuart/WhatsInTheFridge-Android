2b49472f856fcf4ec6ce02ce06d544d599e92326
Grace Stuart <gracerstuart@gmail.com>
2017-06-09 09:48:52 -0700

    add functionality to clicking Logout

--- app/src/main/java/com/epicodus/whatsinthefridge_android/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/ui/MainActivity.java
@@ -6,10 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.epicodus.whatsinthefridge_android.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -40,6 +42,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+       int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
     }
 
     @Override
