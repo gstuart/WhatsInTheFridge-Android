c6db228ded48b0c0a6347b8d9ec077c34e8ea985
Grace Stuart <gracerstuart@gmail.com>
2017-06-09 09:46:29 -0700

    add logout to overflow menu

--- app/src/main/java/com/epicodus/whatsinthefridge_android/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/ui/MainActivity.java
@@ -4,6 +4,8 @@ import android.graphics.Typeface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -31,6 +33,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mLoginButton.setOnClickListener(this);
         mRegisterLink.setOnClickListener(this);
         mAboutButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
