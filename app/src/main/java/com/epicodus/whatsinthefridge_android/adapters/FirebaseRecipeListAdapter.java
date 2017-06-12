9e26e2bb08a300fe02ed65f3718f00191362b959
Grace Stuart <gracerstuart@gmail.com>
2017-06-12 13:07:07 -0700

    add notifyItemMoved method

--- app/src/main/java/com/epicodus/whatsinthefridge_android/adapters/FirebaseRecipeListAdapter.java
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/adapters/FirebaseRecipeListAdapter.java
@@ -41,11 +41,12 @@ public class FirebaseRecipeListAdapter extends FirebaseRecyclerAdapter<Recipe, F
 
     @Override
     public boolean onItemMove(int fromPosition, int toPosition) {
+        notifyItemMoved(fromPosition, toPosition);
         return false;
     }
 
     @Override
     public void onItemDismiss(int position) {
-
+        getRef(position).removeValue();
     }
 }
