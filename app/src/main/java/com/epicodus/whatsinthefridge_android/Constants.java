b9f684b96ed716479ce7c68d145e7782e3834643
Grace Stuart <gracerstuart@gmail.com>
2017-06-12 13:13:26 -0700

    declare index

--- app/src/main/java/com/epicodus/whatsinthefridge_android/Constants.java
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/Constants.java
@@ -6,4 +6,5 @@ public class Constants {
     public static final String API_INGREDENT_QUERY_PARAMETER = "i";
     public static final String PREFERENCES_INGREDIENT_KEY = "ingredient";
     public static final String FIREBASE_CHILD_RECIPES = "recipes";
+    public static final String FIREBASE_QUERY_INDEX = "index";
 }
