be118b1f9f540a1f2d21db32d28a394c6a41906b
Grace Stuart <gracerstuart@gmail.com>
2017-06-12 13:37:40 -0700

    add childEventListener to listAdapter

--- app/src/main/java/com/epicodus/whatsinthefridge_android/adapters/FirebaseRecipeListAdapter.java
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/adapters/FirebaseRecipeListAdapter.java
@@ -10,19 +10,58 @@ import com.epicodus.whatsinthefridge_android.models.Recipe;
 import com.epicodus.whatsinthefridge_android.util.ItemTouchHelperAdapter;
 import com.epicodus.whatsinthefridge_android.util.OnStartDragListener;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
+
+import java.util.ArrayList;
 
 public class FirebaseRecipeListAdapter extends FirebaseRecyclerAdapter<Recipe, FirebaseRecipeViewHolder>  implements ItemTouchHelperAdapter {
     private DatabaseReference mRef;
     private OnStartDragListener mOnStartDragListener;
     private Context mContext;
+    private ChildEventListener mChildEventListener;
+    private ArrayList<Recipe> mRecipes = new ArrayList<>();
 
-    public FirebaseRecipeListAdapter(Class<Recipe> modelClass, int modelLayout, Class<FirebaseRecipeViewHolder> viewHolderClass, Query ref, OnStartDragListener onStartDragListener, Context context) {
+    public FirebaseRecipeListAdapter(Class<Recipe> modelClass,
+                                     int modelLayout,
+                                     Class<FirebaseRecipeViewHolder> viewHolderClass,
+                                     Query ref,
+                                     OnStartDragListener onStartDragListener,
+                                     Context context) {
         super(modelClass, modelLayout, viewHolderClass, ref);
         mRef = ref.getRef();
         mOnStartDragListener = onStartDragListener;
         mContext = context;
+
+        mChildEventListener = mRef.addChildEventListener(new ChildEventListener() {
+             @Override
+             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                 mRecipes.add(dataSnapshot.getValue(Recipe.class));
+             }
+
+             @Override
+             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+             }
+
+             @Override
+             public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+             }
+
+             @Override
+             public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+             }
+
+             @Override
+             public void onCancelled(DatabaseError databaseError) {
+
+             }
+        });
     }
 
     @Override
