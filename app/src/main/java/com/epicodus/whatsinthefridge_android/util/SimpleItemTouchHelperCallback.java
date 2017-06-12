86c5a5ddfc4c5fa8e53cfa8d69b969bd889c9bb8
Grace Stuart <gracerstuart@gmail.com>
2017-06-12 10:32:01 -0700

    verify code is written correctly

--- app/src/main/java/com/epicodus/whatsinthefridge_android/util/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/util/SimpleItemTouchHelperCallback.java
@@ -23,12 +23,13 @@ public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
     @Override
     public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
         final int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
-        final int swipeFlags =  ItemTouchHelper.START | ItemTouchHelper.END;
+        final int swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END;
         return makeMovementFlags(dragFlags, swipeFlags);
     }
 
     @Override
-    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder source, RecyclerView.ViewHolder target) {
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder source,
+                          RecyclerView.ViewHolder target) {
         if (source.getItemViewType() != target.getItemViewType()) {
             return false;
         }
