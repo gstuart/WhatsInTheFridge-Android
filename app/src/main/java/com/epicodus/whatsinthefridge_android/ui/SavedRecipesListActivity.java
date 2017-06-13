ada2cf9a2905ac28ab0c662026e9e3bb73620f6c
Grace Stuart <gracerstuart@gmail.com>
2017-06-13 10:25:31 -0700

    refactor because of new fragment

--- app/src/main/java/com/epicodus/whatsinthefridge_android/ui/SavedRecipesListActivity.java
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/ui/SavedRecipesListActivity.java
@@ -2,75 +2,13 @@ package com.epicodus.whatsinthefridge_android.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
-
-import com.epicodus.whatsinthefridge_android.Constants;
 import com.epicodus.whatsinthefridge_android.R;
-import com.epicodus.whatsinthefridge_android.adapters.FirebaseRecipeListAdapter;
-import com.epicodus.whatsinthefridge_android.adapters.FirebaseRecipeViewHolder;
-import com.epicodus.whatsinthefridge_android.models.Recipe;
-import com.epicodus.whatsinthefridge_android.util.OnStartDragListener;
-import com.epicodus.whatsinthefridge_android.util.SimpleItemTouchHelperCallback;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-
-public class SavedRecipesListActivity extends AppCompatActivity implements OnStartDragListener {
-    private DatabaseReference mRecipeReference;
-    private FirebaseRecipeListAdapter mFirebaseAdapter;
-    private ItemTouchHelper mItemTouchHelper;
-
-    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+public class SavedRecipesListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
-        ButterKnife.bind(this);
-
-        setUpFirebaseAdapter();
-    }
-
-    private void setUpFirebaseAdapter() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String uid = user.getUid();
-
-        Query query = FirebaseDatabase.getInstance()
-                .getReference(Constants.FIREBASE_CHILD_RECIPES)
-                .child(uid)
-                .orderByChild(Constants.FIREBASE_QUERY_INDEX);
-
-        mFirebaseAdapter = new FirebaseRecipeListAdapter(Recipe.class,
-                R.layout.recipe_list_item_drag,
-                FirebaseRecipeViewHolder.class,
-                query,
-                this,
-                this);
-
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.setAdapter(mFirebaseAdapter);
-
-        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mFirebaseAdapter);
-        mItemTouchHelper = new ItemTouchHelper(callback);
-        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mFirebaseAdapter.cleanup();
-    }
-
-    @Override
-    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
-        mItemTouchHelper.startDrag(viewHolder);
     }
 }
