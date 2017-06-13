dce3e918a7212d9b7d74109d8986f22773c4b7bc
Grace Stuart <gracerstuart@gmail.com>
2017-06-13 14:49:33 -0700

    implement interface

--- app/src/main/java/com/epicodus/whatsinthefridge_android/ui/RecipeList.java
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/ui/RecipeList.java
@@ -3,14 +3,26 @@ package com.epicodus.whatsinthefridge_android.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.epicodus.whatsinthefridge_android.R;
+import com.epicodus.whatsinthefridge_android.models.Recipe;
+import com.epicodus.whatsinthefridge_android.util.OnRecipeSelectedListener;
 
-public class RecipeList extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class RecipeList extends AppCompatActivity implements OnRecipeSelectedListener {
+    private Integer mPosition;
+    ArrayList<Recipe> mRecipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
 
+    }
+
+    @Override
+    public void onRecipeSelected(Integer position, ArrayList<Recipe> recipes) {
+        mPosition = position;
+        mRecipes = recipes;
     }
 
 }
