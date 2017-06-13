ca805fc36642984bde9ceb56dc93fab3b54d5e59
Grace Stuart <gracerstuart@gmail.com>
2017-06-13 14:51:46 -0700

    fix error by correcting capitalization

--- app/src/main/java/com/epicodus/whatsinthefridge_android/util/OnRecipeSelectedListener.java
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/util/OnRecipeSelectedListener.java
@@ -1,11 +1,9 @@
 package com.epicodus.whatsinthefridge_android.util;
 
 import com.epicodus.whatsinthefridge_android.models.Recipe;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public interface OnRecipeSelectedListener {
-    public void OnRecipeSelected(Integer position, ArrayList<Recipe> recipes);
+    public void onRecipeSelected(Integer position, ArrayList<Recipe> recipes);
 
 }
