ea343f0ac1b784befb39b4a750fe6e30ab1a16ec
Grace Stuart <gracerstuart@gmail.com>
2017-06-13 10:35:14 -0700

    change setContentView

--- app/src/main/java/com/epicodus/whatsinthefridge_android/ui/SavedRecipesListActivity.java
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/ui/SavedRecipesListActivity.java
@@ -9,6 +9,6 @@ public class SavedRecipesListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipes);
+        setContentView(R.layout.activity_saved_recipe_list);
     }
 }
