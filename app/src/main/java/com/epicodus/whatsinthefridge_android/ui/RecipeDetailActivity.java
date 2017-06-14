1981aae16c9af33d5b84bd6c51cc29e56a744833
Grace Stuart <gracerstuart@gmail.com>
2017-06-14 07:55:24 -0700

    update viewholder

--- app/src/main/java/com/epicodus/whatsinthefridge_android/ui/RecipeDetailActivity.java
+++ app/src/main/java/com/epicodus/whatsinthefridge_android/ui/RecipeDetailActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.epicodus.whatsinthefridge_android.Constants;
 import com.epicodus.whatsinthefridge_android.R;
 import com.epicodus.whatsinthefridge_android.adapters.RecipePagerAdapter;
 import com.epicodus.whatsinthefridge_android.models.Recipe;
@@ -16,7 +17,6 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class RecipeDetailActivity extends AppCompatActivity {
-
     @Bind(R.id.viewPager) ViewPager mViewPager;
     private RecipePagerAdapter adapterViewPager;
     ArrayList<Recipe> mRecipes = new ArrayList<>();
@@ -27,8 +27,8 @@ public class RecipeDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recipe_detail);
         ButterKnife.bind(this);
 
-        mRecipes = Parcels.unwrap(getIntent().getParcelableExtra("recipes"));
-        int startingPosition = getIntent().getIntExtra("position", 0);
+        mRecipes = Parcels.unwrap(getIntent().getParcelableExtra(Constants.EXTRA_KEY_RECIPES));
+        int startingPosition = getIntent().getIntExtra(Constants.EXTRA_KEY_POSITION, 0);
 
         adapterViewPager = new RecipePagerAdapter(getSupportFragmentManager(), mRecipes);
         mViewPager.setAdapter(adapterViewPager);
